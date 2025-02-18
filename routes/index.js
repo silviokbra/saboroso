5431e0b90124319f28a94ad69478f267d6741ade
Silvio Seferin <silvioseferin@gmail.com>
2025-02-18 19:22:47 -0300

    Organizando o HEADER com includes

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,9 @@ router.get('/', function (req, res, next) {
 router.get('/contacts', function (req, res, next) {
 
   res.render('contacts', {
-    title: 'Contato - Restaurante Saboroso!'
+    title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
 
   });
 
@@ -36,7 +38,9 @@ router.get('/contacts', function (req, res, next) {
 router.get('/menus', function (req, res, next) {
 
   res.render('menus', {
-    title: 'Menus - Restaurante Saboroso!'
+    title: 'Menus - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
 
   });
 
@@ -45,7 +49,9 @@ router.get('/menus', function (req, res, next) {
 router.get('/reservations', function (req, res, next) {
 
   res.render('reservations', {
-    title: 'Reserva - Restaurante Saboroso!'
+    title: 'Reserva - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma mesa!'
 
   });
 
@@ -54,7 +60,9 @@ router.get('/reservations', function (req, res, next) {
 router.get('/services', function (req, res, next) {
 
   res.render('services', {
-    title: 'Serviços - Restaurante Saboroso!'
+    title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
 
   });
 
