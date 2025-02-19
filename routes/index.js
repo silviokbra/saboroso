ddb490cdfd4523d063b823b91387560bc0b5f231
Silvio Seferin <silvioseferin@gmail.com>
2025-02-18 21:56:00 -0300

    Fazendo reserva com método post

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -47,14 +48,52 @@ router.get('/menus', function (req, res, next) {
 
 router.get('/reservations', function (req, res, next) {
 
-  res.render('reservations', {
-    title: 'Reserva - Restaurante Saboroso!',
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma mesa!'
+  reservations.render(req, res)
 
-  });
 
 });
+
+
+router.post('/reservations', function (req, res, next) {
+
+
+  if (!req.body.name) {
+
+    reservations.render(req, res, "Preencha o campo Nome.");
+
+  } else if (!req.body.email) {
+
+    reservations.render(req, res, "Preencha o campo Email.");
+
+  } else if (!req.body.people) {
+
+    reservations.render(req, res, "Preencha o numero de pessoas");
+
+  } else if (!req.body.date) {
+
+    reservations.render(req, res, "Selecione a data");
+
+  } else if (!req.body.time) {
+
+    reservations.render(req, res, "Selecione a hora");
+
+  } else {
+
+    reservations.save(req.body).then(results => {
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+    }).catch(err => {
+
+      reservations.render(req, res, err);
+    })
+
+  }
+
+});
+
 
 router.get('/services', function (req, res, next) {
 
