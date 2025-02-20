61dd738118f0fce75c3774e48e008a08e1144e31
Silvio Seferin <silvioseferin@gmail.com>
2025-02-20 20:32:39 -0300

    Admin da reserva CREATE

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require("express");
 var users = require("./../inc/users");
 var admin = require("./../inc/admin");
 var menus = require("./../inc/menus");
+var reservations = require("./../inc/reservations");
 var router = express.Router();
 
 router.use(function (req, res, next) {
@@ -138,6 +139,32 @@ router.get("/reservations", function (req, res, next) {
     res.render("admin/reservations", admin.getParams(req, {
         date: {}
     }));
+
+});
+
+router.post("/reservations", function (req, res, next) {
+
+    reservations.save(req.fields, req.files).then(results => {
+
+        res.send(results);
+
+    }).catch(err => {
+
+        res.send(err);
+    });
+
+});
+
+router.delete("/reservations/:id", function (req, res, next) {
+
+    reservations.delete(req.params.id).then(results => {
+
+        res.send(results);
+
+    }).catch(err => {
+
+        res.send(err);
+    });
 
 });
 
