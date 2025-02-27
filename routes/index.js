4ee38c215b44fb694e2b279349b3c67b4e009799
Silvio Seferin <silvioseferin@gmail.com>
2025-02-27 02:09:07 -0300

    Adm e-mails

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
 var contacts = require('./../inc/contacts');
+var emails = require('./../inc/emails');
 var router = express.Router();
 
 /* GET home page. */
@@ -125,6 +126,18 @@ router.get('/services', function (req, res, next) {
 
   });
 
+});
+
+
+router.post("/subscribe", function (req, res, next) {
+
+  emails.save(req).then(results => {
+
+    res.send(results);
+
+  }).catch(err => {
+    res.send(err);
+  });
 });
 
 module.exports = router;
